f998d9de569a872bb35ea6ca77c34e663870275f
Atik Shakur <atik.shakur98@gmail.com>
2019-02-23 17:24:11 +0600

    10929 - You can say 11

---
+++ YouCanSayEleven.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class YouCanSayEleven {
+    // 10929 - You can say 11
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        BigInteger e = new BigInteger("11");
+
+        while (!s.equals("0")) {
+            BigInteger b = new BigInteger(s);
+
+            if (b.equals(BigInteger.ZERO))
+                break;
+
+            if (b.mod(e).equals(BigInteger.ZERO))
+                System.out.println(s + " is a multiple of 11.");
+            else
+                System.out.println(s + " is not a multiple of 11.");
+
+            s = scanner.next();
+        }
+
+    }
+}
